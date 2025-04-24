0b76c045c2bfa21fe625292a9f5dd372f65208f4
PEDRO VICTOR RODRIGUES DE ALMEIDA <24122478@dsn127m1242314.cfp127aluno.local>
2025-04-24 17:16:17 -0300

    adicionando 2 tela

---
+++ app/src/main/java/br/senai/sp/jandira/lion_school/screens/CoursesCards.kt
@@ -0,0 +1,106 @@
+package br.senai.sp.jandira.lion_school.screens
+
+import android.widget.Space
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.lion_school.R
+
+@Composable
+fun CourseCard(
+    cursoNome: String = "SIGLA",
+    TextGrande: String = "Nome do curso",
+    TextPequeno: String = "Descrição",
+    Time: String = "Tempo de curso",
+    bulletImg: Painter = painterResource(R.drawable.galeria),
+
+    ) {
+
+    Card(
+        modifier = Modifier
+            .width(322.dp)
+            .height(209.dp),
+        shape = RoundedCornerShape(15.dp),
+        border = BorderStroke(
+            1.dp,
+            Color(0xFFFFC23D)
+        )
+    )
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.horizontalGradient(
+                        listOf(
+                            Color(0xFF3347B0),
+                            Color(0xFFCFD4EA)
+                        )
+                    )
+                )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(18.dp),
+                verticalArrangement = Arrangement.SpaceAround
+            )
+            {
+
+                Row(
+                    modifier = Modifier
+                        .padding(start = 20.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Image(
+                        painter = bulletImg, contentDescription = "",
+                        modifier = Modifier
+                            .size(90.dp)
+                    )
+                    Spacer(modifier = Modifier .width(20.dp))
+                    Text(text = cursoNome,
+                        fontSize = 64.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color(0xFFC23D)
+
+                    )
+
+
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CoursersCardsPreview() {
+    CourseCard()
+}
---
+++ app/src/main/java/br/senai/sp/jandira/lion_school/screens/CoursesScreen.kt
@@ -0,0 +1,135 @@
+package br.senai.sp.jandira.lion_school.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.lion_school.R
+
+@Composable
+fun CoursesScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 30.dp, start = 10.dp, end = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = stringResource(R.string.LOGO),
+                    modifier = Modifier
+                        .width(40.dp)
+                        .height(56.dp)
+                        .padding(vertical = 2.dp)
+                )
+                Text(
+                    text = stringResource(R.string.NOME),
+                    fontSize = 15.sp,
+                    color = Color(0xFF3347B0),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .width(50.dp)
+                        .padding(bottom = 10.dp)
+                )
+
+            }
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(top = 5.dp, bottom = 10.dp)
+
+            )
+
+            OutlinedTextField(
+                value = "", onValueChange = {},
+                modifier = Modifier
+
+                    .height(70.dp)
+                    .fillMaxWidth()
+                    .padding(top = 20.dp),
+                shape = RoundedCornerShape(10.dp),
+                label = {
+                    Text(text = stringResource(R.string.findC))
+                },
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "",
+                    )
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = Color(0xFFFFC23D),
+                    unfocusedContainerColor = Color(0xFFF5F5F5)
+                )
+
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 210.dp, start = 15.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.lion_lista),
+                contentDescription = "",
+                modifier = Modifier
+                    .width(35.dp)
+                    .height(36.dp)
+            )
+            Text(text = stringResource(id = R.string.courses),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF3347B0)
+            )
+
+        }
+    }
+
+}
+
+
+@Composable
+@Preview
+private fun CoursesScreenPreview() {
+    CoursesScreen()
+}
